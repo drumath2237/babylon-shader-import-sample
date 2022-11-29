a6696be4cc849acbefba305077a1bb010ee8e7a1
drumath2237 <2237k.drumath@gmail.com>
2022-11-29 22:38:33 +0900

    :sparkles: babylonによる豆腐シーンの作成

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Engine, MeshBuilder, Scene, Vector3 } from '@babylonjs/core';
 import './style.scss';
 
 const main = () => {
@@ -8,7 +9,21 @@ const main = () => {
     return;
   }
 
-  console.log('hi!');
+  renderCanvas.width = window.innerWidth;
+  renderCanvas.height = window.innerHeight;
+
+  const engine = new Engine(renderCanvas, true);
+  const scene = new Scene(engine);
+
+  scene.createDefaultCameraOrLight(true, true, true);
+  scene.createDefaultEnvironment();
+
+  const box = MeshBuilder.CreateBox('box', { size: 0.1 }, scene);
+  box.position = new Vector3(0, 0.05, 0);
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
 };
 
 main();
